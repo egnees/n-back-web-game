8a196efcb7591eecc62c623bc9545aa64fa65fef
Sergei Yakovlev <3gnees@gmail.com>
2022-12-30 08:52:16 +0300

    added button block after pressing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var rows = new Array()
-var columns = new Array()
+var cols = new Array()
 var lets = new Array();
+
+var gameStarted = false;
 
 var iterationsCount;
 var needIterationsCount;
+var needMatchCount;
 
 var currentRow;
 var currentCol;
@@ -33,34 +36,37 @@ function playLetterSound() {
 }
 
 function generateCurrentCell() {
-    iterationsCount++;
     console.log("round " + iterationsCount);
 
     soundButtonPressed = false;
     positionButtonPressed = false;
 
+    soundButton.disabled = false;
+    positionButton.disabled = false;
+
     if (iterationsCount == needIterationsCount) {
         clearInterval(intervalId);
 
+        console.log("corr pos/corr sound: " + correctPosAnswers + "/" + correctSoundAnswers);
+        console.log("incorr pos/incorr sound: " + incorrectPosAnswers + "/" + incorrectSoundAnswers);
+
         console.log("correct answers: " + (correctPosAnswers + correctSoundAnswers));
         console.log("incorrect answers: " + (incorrectPosAnswers + incorrectSoundAnswers));
+
+        gameStarted = false;
+
+        return;
     }
 
-    rows.push(currentRow);
-    columns.push(currentCol);
-
-    var num = Math.round(Math.random() * 8);
-    currentRow = Math.floor(num / 3) + 1;
-    currentCol = num % 3 + 1;
-
-    var letterId = Math.round(Math.random() * (letters.length - 1));
-    currentLetter = letters[letterId];
-    lets.push(currentLetter);
-
+    currentLetter = lets[iterationsCount];
     playLetterSound();
+
+    currentRow = rows[iterationsCount];
+    currentCol = cols[iterationsCount];
 
     currentCell = document.getElementById("grid-cell-" + currentRow + "-" + currentCol);
     currentCell.style.backgroundColor = "blue";
+
     setTimeout(() => {
         currentCell.style.backgroundColor = "white";
     }, 750);
@@ -85,53 +91,129 @@ function generateCurrentCell() {
         console.log("i checked correctness right now");
         console.log("checkPosition: " + checkPosition());
         console.log("checkSound: " + checkSound());
+
+        iterationsCount++;
     }, 1900);
 }
 
 function checkPosition() {
-    if (rows.length >= 3 && rows[rows.length - 3] == currentRow && columns[columns.length - 3] == currentCol) {
+    if (iterationsCount >= 3 && rows[iterationsCount - 3] == currentRow && cols[iterationsCount - 3] == currentCol) {
         return true;
     } 
     return false;
 }
 
 function checkSound() {
-    if (lets.length >= 3 && lets[lets.length - 3] == currentLetter) {
+    if (iterationsCount >= 3 && lets[iterationsCount - 3] == currentLetter) {
         return true;
     } 
     return false;
 }
 
 function positionButtonClicked() {
+    if (!gameStarted) {
+        return;
+    }
     positionButton.disabled = true;
     positionButtonPressed = true;
-    setTimeout(() => {
-        console.log("making position button enabled");
-        positionButton.disabled = false;
-    }, 1000);
 }
 
 function soundButtonClicked() {
+    if (!gameStarted) {
+        return;
+    }
     soundButton.disabled = true;
     soundButtonPressed = true;
-    setTimeout(() => {
-        console.log("making sound button enabled");
-        soundButton.disabled = false;
-    }, 1000);
 }
 
 function startGame() {
     iterationsCount = 0;
-    needIterationsCount = 10;
+    needIterationsCount = 20;
+    needMatchCount = 5;
     correctPosAnswers = 0;
     incorrectPosAnswers = 0;
     correctSoundAnswers = 0;
     incorrectSoundAnswers = 0;
     soundButtonPressed = false;
     positionButtonPressed = false;
+    
+    console.log("generating positions...");
+    generatePositions(needIterationsCount, needMatchCount);
+    console.log("rows: " + rows);
+    console.log("cols: " + cols);
+
+    console.log("generating letters...");
+    generateLetters(needIterationsCount, needMatchCount);
+    console.log("lets: " + lets);
+
+    gameStarted = true;
+
     intervalId = setInterval(generateCurrentCell, 2000);
 
     console.log("game started");
+}
+
+function generateArray0(n, r) {
+    s = new Set();
+    while (s.size < n) {
+        var num = Math.round(Math.random() * r);
+        s.add(num);
+    }
+    var arr = new Array();
+    arr = Array.from(s);
+    arr.sort(function(a, b) {
+        return a - b;
+    });
+    console.log("generated array 0: " + arr);
+    return arr;
+}
+
+function generateArray(n, l, r) {
+    arr = generateArray0(n, r - l);
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] += l;
+    }
+    console.log("generated array: " + arr);
+    return arr;
+}
+
+function generateArrayWithReplays0(n, r) {
+    var arr = new Array();
+    for (var i = 0; i < n; i++) {
+        arr[i] = Math.round(Math.random() * r);
+    }
+    return arr;
+}
+
+function generateArrayWithReplays(n, l, r) {
+    var arr = generateArrayWithReplays0(n, r - l);
+    for (var i = 0; i < n; i++) {
+        arr[i] += l;
+    }
+    return arr;
+}
+
+function generatePositions(total, match) {
+    var matched = generateArray(match, 3, total - 1);
+    rows = generateArrayWithReplays(total, 1, 3);
+    cols = generateArrayWithReplays(total, 1, 3);
+    for (var i = 0; i < match; i++) {
+        var x = matched[i];
+        rows[x] = rows[x - 3];
+        cols[x] = cols[x - 3];
+    }
+}
+
+function generateLetters(total, match) {
+    var matched = generateArray(match, 3, total - 1);
+    lets = generateArrayWithReplays0(total, letters.length - 1);
+    for (var i = 0; i < match; ++i) {
+        var x = matched[i];
+        lets[x] = lets[x - 3];
+    }
+    for (var i = 0; i < total; i++) {
+        lets[i] = letters[lets[i]];
+    }
 }
 
 document.addEventListener("keypress", function(event) {
